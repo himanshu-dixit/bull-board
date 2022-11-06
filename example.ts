33f7132568077b9e524180e5142697f6237b404e
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-11-06 13:55:48 +0530

    Update example.ts

--- example.ts
+++ example.ts
@@ -1,12 +1,17 @@
 // @ts-nocheck
-import * as Bull from 'bull';
-import Queue3 from 'bull';
-import { Queue as QueueMQ, QueueScheduler, Worker } from 'bullmq';
-import express from 'express';
-import { BullMQAdapter } from '@bull-board/api/src/queueAdapters/bullMQ';
-import { BullAdapter } from '@bull-board/api/src/queueAdapters/bull';
-import { createBullBoard } from '@bull-board/api/src';
-import { ExpressAdapter } from '@bull-board/express/src';
+const express = require('express');
+const Queue = require('bull');
+const QueueMQ = require('bullmq');
+const { createBullBoard } = require('@bull-board/api');
+const { BullAdapter } = require('@bull-board/api/bullAdapter');
+const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
+const { ExpressAdapter } = require('@bull-board/express');
+
+const TEST_EXECUTION_QUEUE = "TEST_EXECUTION_QUEUE";
+const VIDEO_PROCESSOR_QUEUE = "VIDEO_PROCESSOR_QUEUE";
+const TEST_COMPLETE_QUEUE = "TEST_COMPLETE_QUEUE";
+
+
 
 const redisOptions: any = {
   port: process.env.REDIS_PORT,
@@ -15,90 +20,28 @@ const redisOptions: any = {
   password: process.env.REDIS_PASSWORD,
 };
 
-const sleep = (t: number) => new Promise((resolve) => setTimeout(resolve, t * 1000));
 
-const createQueue3 = (name: string) => new Queue3(name, { redis: redisOptions });
-const createQueueMQ = (name: string) => new QueueMQ(name, { connection: redisOptions });
+const testExecutionQueue = new Queue(TEST_EXECUTION_QUEUE, redisOptions);
+const videoProcessorQueue = new Queue(VIDEO_PROCESSOR_QUEUE, redisOptions);
+const testCompleteQueue = new Queue(TEST_COMPLETE_QUEUE, redisOptions);
 
-function setupBullProcessor(bullQueue: Bull.Queue) {
-  bullQueue.process(async (job) => {
-    for (let i = 0; i <= 100; i++) {
-      await sleep(Math.random());
-      await job.progress(i);
-      await job.log(`Processing job at interval ${i}`);
-      if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
-    }
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+;
 
-    return { jobId: `This is the return value of job (${job.id})` };
-  });
-}
+const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
+  queues: [new BullAdapter(testExecutionQueue), new BullAdapter(videoProcessorQueue), new BullMQAdapter(testCompleteQueue)],
+  serverAdapter: serverAdapter,
+});
 
-async function setupBullMQProcessor(queueName: string) {
-  const queueScheduler = new QueueScheduler(queueName, {
-    connection: redisOptions,
-  });
-  await queueScheduler.waitUntilReady();
+const app = express();
 
-  new Worker(
-    queueName,
-    async (job) => {
-      for (let i = 0; i <= 100; i++) {
-        await sleep(Math.random());
-        await job.updateProgress(i);
-        await job.log(`Processing job at interval ${i}`);
+app.use('/admin/queues', serverAdapter.getRouter());
 
-        if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
-      }
-
-      return { jobId: `This is the return value of job (${job.id})` };
-    },
-    { connection: redisOptions }
-  );
-}
-
-const run = async () => {
-  const app = express();
-
-  const exampleBull = createQueue3('ExampleBull');
-  const exampleBullMq = createQueueMQ('ExampleBullMQ');
-
-  await setupBullProcessor(exampleBull); // needed only for example proposes
-  await setupBullMQProcessor(exampleBullMq.name); // needed only for example proposes
-
-  app.use('/add', (req, res) => {
-    const opts = req.query.opts || ({} as any);
-
-    if (opts.delay) {
-      opts.delay = +opts.delay * 1000; // delay must be a number
-    }
-
-    exampleBull.add({ title: req.query.title }, opts);
-    exampleBullMq.add('Add', { title: req.query.title }, opts);
-
-    res.json({
-      ok: true,
-    });
-  });
-
-  const serverAdapter: any = new ExpressAdapter();
-  serverAdapter.setBasePath('/ui');
-
-  createBullBoard({
-    queues: [new BullMQAdapter(exampleBullMq), new BullAdapter(exampleBull)],
-    serverAdapter,
-  });
-
-  app.use('/ui', serverAdapter.getRouter());
-
-  app.listen(3000, () => {
-    console.log('Running on 3000...');
-    console.log('For the UI, open http://localhost:3000/ui');
-    console.log('Make sure Redis is running on port 6379 by default');
-    console.log('To populate the queue, run:');
-    console.log('  curl http://localhost:3000/add?title=Example');
-    console.log('To populate the queue with custom options (opts), run:');
-    console.log('  curl http://localhost:3000/add?title=Test&opts[delay]=10');
-  });
-};
-
-run().catch((e) => console.error(e));
+// other configurations of your server
+  
+app.listen(3000, () => {
+  console.log('Running on 3000...');
+  console.log('For the UI, open http://localhost:3000/admin/queues');
+  console.log('Make sure Redis is running on port 6379 by default');
+});
